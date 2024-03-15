dcf57c707ed6e7e5a13a4bc0734b1e1c0072d1f3
Surendar302003 <159808184+Surendar302003@users.noreply.github.com>
2024-03-15 07:50:41 -0700

    Add files via upload

---
+++ To-do list app.py
@@ -0,0 +1,88 @@
+#1 import module
+from tkinter import *
+from tkinter import messagebox
+
+#5 create function
+def newTask():
+    task = my_entry.get()
+    if task != "":
+        lb.insert(END, task)
+        my_entry.delete(0, "end")
+    else:
+        messagebox.showwarning("WARNING", "Please enter some task :) ")
+
+def deleteTask():
+    lb.delete(ANCHOR)
+
+#2 configure & create main window    
+ws = Tk()
+ws.geometry('500x450+500+200')
+ws.title('TO-DO LIST :)')
+ws.config(bg='#223441')
+ws.resizable(width=False, height=False)
+
+#4 create widgets(frame, listbox, scrollbar, Entry, button)
+frame = Frame(ws)
+frame.pack(pady=10)
+
+lb = Listbox(
+    frame,
+    width=25,
+    height=8,
+    font=('Times', 18),
+    bd=0,
+    fg='#464646',
+    highlightthickness=0,
+    selectbackground='#a6a6a6',
+    activestyle="none",
+    
+)
+lb.pack(side=LEFT, fill=BOTH)
+
+task_list = [
+    
+    ]
+
+for item in task_list:
+    lb.insert(END, item)
+
+sb = Scrollbar(frame)
+sb.pack(side=RIGHT, fill=BOTH)
+
+lb.config(yscrollcommand=sb.set)
+sb.config(command=lb.yview)
+
+my_entry = Entry(
+    ws,
+    font=('times', 24)
+    )
+
+my_entry.pack(pady=20)
+
+button_frame = Frame(ws)
+button_frame.pack(pady=20)
+
+addTask_btn = Button(
+    button_frame,
+    text='Add Task',
+    font=('times 14'),
+    bg='#c5f776',
+    padx=20,
+    pady=10,
+    command=newTask
+)
+addTask_btn.pack(fill=BOTH, expand=True, side=LEFT)
+
+delTask_btn = Button(
+    button_frame,
+    text='Delete Task',
+    font=('times 14'),
+    bg='#ff8b61',
+    padx=20,
+    pady=10,
+    command=deleteTask
+)
+delTask_btn.pack(fill=BOTH, expand=True, side=LEFT)
+
+#3 Create main loop
+ws.mainloop()
